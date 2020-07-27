b2f0abd1f7030d09524f92098f8a0f2281644ccc
Camila Yanez <ccynzz@gmail.com>
2020-07-26 19:59:40 -0500

    maqueta registro y login

--- src/main.js
+++ src/main.js
@@ -1,5 +1,54 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { myFunction } from "./lib/index.js";
 
 myFunction();
+
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = document.querySelector(".inputName").value;
+  const email = document.querySelector(".email-signup").value;
+  const password = document.querySelector(".password-signup").value;
+  console.log(email, name);
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // [START_EXCLUDE]
+      if (errorCode == "auth/weak-password") {
+        alert("The password is too weak.");
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+      // [END_EXCLUDE]
+    });
+  alert("Welcome, successfully registered user");
+});
+
+function toggleRegisterLogin() {}
+
+const alreadyAccount = document.getElementById("alreadyAccount");
+const containerName = document.querySelector(".inputName");
+const btnSign = document.querySelector(".btn-signup");
+const btnSignGoogle = document.querySelector(".btn-signup-google");
+alreadyAccount.addEventListener("click", function (e) {
+  console.log(containerName.className);
+  containerName.classList.toggle("hide");
+  if (containerName.className == "inputName hide") {
+    alreadyAccount.textContent = "Don’t have an account? Sign Up";
+    btnSign.textContent = "SIGN IN";
+    btnSignGoogle.textContent = "SIGN IN WITH GOOGLE";
+  } else {
+    alreadyAccount.textContent = "Already have an account? Sign In";
+    btnSign.textContent = "SIGN UP";
+    btnSignGoogle.textContent = "REGISTER WITH GOOGLE";
+  }
+
+  console.log("hola");
+});
