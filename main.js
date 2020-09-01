59dbac0d781d809c33cf6399404ca771991b4c00
Malejandraib <mariaibanez07@gmail.com>
2020-08-31 22:42:40 -0500

    Updates

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-// Este es el punto de entrada de tu aplicacion 
-// Aquí importamos la función de ruteo 
-import{router} from  "./router.js";
+// Este es el punto de entrada de tu aplicacion
+// Aquí importamos la función de ruteo
+/* eslint-disable */
+import {router} from './router.js';
 
 const init = () => {
-  router(window.location.hash);
-  
-  window.addEventListener("hashchange", () => {
-      router(window.location.hash);
-  });
+	router(window.location.hash);
+
+	window.addEventListener('hashchange', () => {
+		router(window.location.hash);
+	});
 };
 
-window.addEventListener("load", init); 
+window.addEventListener('load', init);
--- router.js
+++ router.js
@@ -1,52 +1,53 @@
-//Aquí importamos el objeto que manipula los js de las vistas 
-import { myFunction } from "./lib/index.js"; myFunction();
+/* eslint-disable */
 import signup from './lib/signup.js';
 import signin from './lib/signin.js';
 import timeline from './lib/timeline.js';
 import notfound from './lib/notfound.js';
 import profile from './lib/profile.js';
-//import {changeState} from './lib/firebasefunction.js';
+import events from './lib/events.js'
 
 const pages = {
     signup,
     signin,
     timeline,
     profile,
-    notfound
+    notfound,
+    events
 };
 
-export const router = (route) =>{
+export const router = (route) => {
+    const content = document.getElementById('root');
+    firebase.auth().onAuthStateChanged(function (user) {
+        content.innerHTML = '';
+        if (user) {
+            switch (route) {
+                case '':
+                    return pages.signup();
+                case '#signin':
+                    return pages.signin();
+                case '#timeline':
+                    return pages.timeline(); //acción que dce hash = timeline  
+                case '#profile':
+                    return pages.profile();
+                case '#events':
+                    return pages.events();
 
-    let content = document.getElementById('root');
-            firebase.auth().onAuthStateChanged(function(user) {
-                content.innerHTML = "";
-                if (user) {      
-                    switch(route) {
-                        case '':
-                            //puede hacer un redirect a timeline de una                            
-                            return pages.signup();
-                        case '#signin':
-                            return pages.signin();
-                        case '#timeline':
-                            return pages.timeline(); //acción que dce hash = timeline  
-                        case '#profile':
-                            return pages.profile();
-                        case '#help':
-                            return content.innerHTML = "<p>Help</p>";
-    
-                            default:
-                            return pages.notfound();
-                    }
-                } else {
-                    switch(route) {
-                        case '':
-                            return pages.signup();
-                        case '#signin':
-                            return pages.signin(); 
-                        default:
-                            return pages.notfound();
-                    }
-                }
-            });  
+                case '#help':
+                    return content.innerHTML = '<p>Help</p>';
+
+                default:
+                    return pages.notfound();
+            }
+        } else {
+            switch (route) {
+                case '':
+                    return pages.signup();
+                case '#signin':
+                    return pages.signin();
+                default:
+                    return pages.notfound();
+            }
+        }
+    });
 }
 
